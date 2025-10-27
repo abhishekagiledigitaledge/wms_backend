658e47581aae7fd512f23132c7c9ac1d6f819354
abhishekagiledigitaledge <abhishek@agiledigitaledge.com>
2025-10-28 02:17:17 +0530

    change

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use("/webhooks", bodyParser.raw({ type: "*/*" }));
+app.use(express.raw({ type: "application/json" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
